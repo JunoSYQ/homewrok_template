b48a4e47179787500e9c656eb5334548e2fd2195
JunoSYQ <104131107+JunoSYQ@users.noreply.github.com>
2022-07-06 01:44:48 +0800

    Update BubbleSort.py

--- 分治算法/BubbleSort.py
+++ 分治算法/BubbleSort.py
@@ -1,1 +1,12 @@
-
+def BubbleSort(list,n):
+    global BubbleCount
+    numberOfPairs = n
+    swappedElements = True
+    while swappedElements:
+        numberOfPairs = numberOfPairs - 1
+        swappedElements = False
+        for i in range(numberOfPairs):
+            BubbleCount = BubbleCount+1
+            if list[i] > list[i + 1]:
+                list[i], list[i + 1] = list[i + 1], list[i]
+                swappedElements = True
