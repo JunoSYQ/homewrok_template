584f8a33c831a81f08062e07c58a1f399219ebba
JunoSYQ <104131107+JunoSYQ@users.noreply.github.com>
2022-07-06 13:37:18 +0800

    Update Radom.py

--- 分治算法/Radom.py
+++ 分治算法/Radom.py
@@ -1,1 +1,8 @@
-
+def Radom(list,n):
+    A = 2
+    B = 5
+    seed = random.randint(0, 10)
+    M = 101
+    list[0] = (A*seed+B) % M
+    for i in range(n-1):
+        list[i+1] = (A*list[i]+B) % M
